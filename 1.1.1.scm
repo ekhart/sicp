5ce65c98b97c2e7845f566d58dd8df846e2241b0
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-10 00:21:04 +0200

    first examples

---
+++ 1.1.1.scm
@@ -0,0 +1,9 @@
+; (defmacro (print 
+; todo: print macro: body string = evaluated body
+
+(display 486)
+; (display "486 = " 486) ; dont work because of 2. argument.
+(newline)
+
+(display (+ 137 349))
+(newline)
