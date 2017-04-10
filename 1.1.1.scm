0e73368b005586d8b8fe73f301df2bb15812945a
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-10 17:20:00 +0200

    comment

--- 1.1.1.scm
+++ 1.1.1.scm
@@ -1,18 +1,18 @@
 ; (defmacro (print 
 ; todo: print macro: body string = evaluated body
 
-(display 486)
+; (display 486)
 ; (display "486 = " 486) ; dont work because of 2. argument.
-(newline)
+; (newline)
 
-(display (+ 137 349))
-(newline)
+; (display (+ 137 349))
+; (newline)
 
-(display (- 1000 334))
-(newline)
+; (display (- 1000 334))
+; (newline)
 
-(display (* 5 99))
-(newline)
+; (display (* 5 99))
+; (newline)
 
 (display (/ 10 5))
 (newline)
@@ -33,6 +33,6 @@
 
 
 (display (+ (* 3 (+ (* 2 4) (+ 3 5))) (+ (- 10 7) 6)))
-(newline)	 
+(newline)
 
 	 
