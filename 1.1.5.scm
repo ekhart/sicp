78f0e9e99a233c17a8b42283e2ab068cf527ffb7
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-12 01:47:35 +0200

    simpliced model

---
+++ 1.1.5.scm
@@ -0,0 +1,19 @@
+(f 5)
+
+(sum-of-squares (+ a 1) (* a 2))
+
+(sum-of-squares (+ 5 1) (* 5 2))
+
+(sum-of-squares 6 7)
+
+(+ (* 6 6) (* 7 7))
+
+(+ 36 100)
+
+136
+
+; normal
+(f 5)
+
+(sum-of-squares (+ 5 1) (* 5 2))
+
