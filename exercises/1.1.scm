70e280fd1212de30c78efb4cd6dfca49f6add254
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-14 00:37:09 +0200

    macro, and experiments

--- exercises/1.1.scm
+++ exercises/1.1.scm
@@ -1,6 +1,20 @@
 (load "../utils.scm")
-; (require 'macro)
 
-(peae (+ 1 1))
 
-; (print 1)
+(peae 10)
+(peae (+ 5 3 4))
+(peae (- 9 1))
+(peae (/ 6 2))
+(peae (+ (* 2 4) (- 4 6)))
+ 
+; (peae (define a 3))
+; (peae (define b (+ a 1))) ; gives error - eval in local env?
+(define a 3)
+(define b (+ a 1))
+; (map (lambda (x) (peae x)) '(a b)) ; dont work, cant find x?
+; (dolist (x '(a b))
+; 	(peae x))
+; cant find symbol 
+(peae (+ a b (* a b)))
+
+; (map peae '(10 (- 9 1)))
--- utils.scm
+++ utils.scm
@@ -1,3 +1,10 @@
 (define (print x)
   (display x)
   (newline))
+
+; print expression and eval it
+(define-macro (peae e)
+  (display e)
+  (display " => ")
+  (display (eval e))
+  (newline))
