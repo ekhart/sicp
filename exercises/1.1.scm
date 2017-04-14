7fe536dd20d37c4d257269fe666d73d8899c753c
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-14 16:35:48 +0200

    solved

--- exercises/1.1.scm
+++ exercises/1.1.scm
@@ -18,3 +18,21 @@
 (peae (+ a b (* a b)))
 
 ; (map peae '(10 (- 9 1)))
+
+(peae (= a b))
+
+(peae (if (and (> b a) (< b (* a b)))
+	  b
+	  a))
+
+(peae (cond ((= a 4) 6)
+	    ((= b 4) (+ 6 7 a))
+	    (else 25)))
+
+(peae (+ 2 (if (> b a) b a)))
+
+(peae (* (cond ((> a b) a)
+	       ((< a b) b)
+	       (else -1))
+	 (+ a 1)))
+
