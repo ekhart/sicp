0eb9c82cfe087c251975d29ca6f3e149e0bcdca7
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-25 18:24:21 +0200

    newton sqrt algorithm

--- 1.1/1.1.7.scm
+++ 1.1/1.1.7.scm
@@ -1,3 +1,28 @@
+(load "../utils.scm")
+
 ;; (define (sqrt x)
 ;;   (takie-ze y (and (>= y 0)
 ;; 		   (= (square y) x))))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+		 x)))
+
+(define (improve guess x)
+  (average guess (/ guess x)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.0001))
+
+(define (square x)
+  (* x x))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(peae (sqrt 9))
