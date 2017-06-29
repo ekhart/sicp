f3763ae99b39d523c05f919f1ca90e27127936ca
Artur Dębkowski <arturdebkowski@gmail.com>
2017-06-29 19:39:44 +0200

    large numbers

--- exercises/1.7.scm
+++ exercises/1.7.scm
@@ -7,3 +7,9 @@
 (peae (sqrt 0.000001))
 ; always return if x < 0.001
 ; ts> (sqrt 1e-05) => 0.03135649011
+
+(peae (sqrt 100))
+(peae (sqrt 10000))
+(peae (sqrt 1000000))
+(peae (sqrt 100000000))
+(peae (sqrt 10000000000))
